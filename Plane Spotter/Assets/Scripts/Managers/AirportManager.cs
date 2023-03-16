2f8b8a934047fce33714c59864a67c5b8a623849
Matthew Burke <mb6457@cs.ship.edu>
2023-03-15 21:12:25 -0400

    fuck you

--- Plane Spotter/Assets/Scripts/Managers/AirportManager.cs
+++ Plane Spotter/Assets/Scripts/Managers/AirportManager.cs
@@ -1,38 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.IO;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Networking;
 
 using gps = GPS;
-public class AirportData
-{
-    public string Code { get; set; }
-    public string Name { get; set; }
-    public double Elevation { get; set; }
-    public double Longitude { get; set; }
-    public double Latitude { get; set; }
+//public class AirportData
+//{
+//    public string Code { get; set; }
+//    public string Name { get; set; }
+//    public double Elevation { get; set; }
+//    public double Longitude { get; set; }
+//    public double Latitude { get; set; }
 
-    public AirportData(string code, string name, double elevation, double longitude, double latitude)
-    {
-        this.Code = code;
-        this.Name = name;
-        this.Elevation = elevation;
-        this.Longitude = longitude;
-        this.Latitude = latitude;
-    }
+//    public AirportData(string code, string name, double elevation, double longitude, double latitude)
+//    {
+//        this.Code = code;
+//        this.Name = name;
+//        this.Elevation = elevation;
+//        this.Longitude = longitude;
+//        this.Latitude = latitude;
+//    }
+//}
+
+[Serializable]
+public struct AirportData
+{
+    public string Code;
+    public string Name;
+    public double Elevation;
+    public double Latitude;
+    public double Longitude;
 }
 
+[Serializable]
 public class AirportSet
 {
     public List<AirportData> airports = new List<AirportData>();
-    
-    public void addAirport(AirportData airport)
-    {
-        airports.Add(airport);
-    }
-
-    public List<AirportData> GetAirports() { return airports; }
-
 }
 
 public class AirportManager : MonoBehaviour
@@ -47,27 +57,16 @@ public class AirportManager : MonoBehaviour
 
     public GameObject AirportBaseObject;
 
+    public string FlightAwareKey;
+
+    public double Radius;
 
     // Start is called before the first frame update
     void Start()
     {
-        airports.addAirport(new AirportData("N68", "Franklin County Rgnl", 687.9, -77.6432563, 39.9729617));
-        airports.addAirport(new AirportData("W05", "Gettysburg Rgnl", 553.2, -77.27465, 39.8413092));
-        airports.addAirport(new AirportData("N94", "Carlisle", 510.1, -77.1742736, 40.1879144));
-        airports.addAirport(new AirportData("W73", "Mid Atlantic Soaring Center", 573, -77.3513761, 39.75704));
-        airports.addAirport(new AirportData("KHGR", "Hagerstown Rgnl", 703.1, -77.7265, 39.7085));
-        airports.addAirport(new AirportData("07N", "Bermudian Valley Airpark", 470, -77.0038667, 40.0167617));
-        airports.addAirport(new AirportData("KTVH", "York", 494.7, -76.8730278, 39.917));
-        airports.addAirport(new AirportData("0P8", "Lazy B Ranch", 476, -76.8153, 40.0244236));
-        airports.addAirport(new AirportData("KCXY", "Capital City", 346.7, -76.8513611, 40.2171389));
-        airports.addAirport(new AirportData("P34", "Mifflintown", 545, -77.4056667, 40.5989444));
-        airports.addAirport(new AirportData("KDMW", "Carroll Co Rgnl", 789.2, -77.0076667, 39.6082778));
-        airports.addAirport(new AirportData("KMDT", "Harrisburg Intl", 310, -76.7626192, 40.1931917));
-        airports.addAirport(new AirportData("W35", "Potomac Airpark", 412.5, -78.1660833, 39.6926111));
-        airports.addAirport(new AirportData("P17", "Greater Breezewood Rgnl", 1345, -78.2977333, 39.8742681));
-        // misc
-        airports.addAirport(new AirportData("KBWI", "Baltimore/Washington Intl Airport", 143.4, -76.669, 39.176));
 
+        airports = GetAirportsFromFA(FlightAwareKey, 40.0506496, -77.5275351, 50);
+        
         foreach(AirportData airport in airports.airports)
         {
             GameObject na = Instantiate(AirportBaseObject);
@@ -96,5 +95,54 @@ public class AirportManager : MonoBehaviour
     public int GetNumAirports()
     {
         return airports.airports.Count;
+    }
+
+    private AirportSet GetAirportsFromFA(string ApiKey, double lat, double lon, double radius)
+    {
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.DefaultRequestHeaders.Accept.Clear();
+        //        client.DefaultRequestHeaders.Accept.Add(
+        //            new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        //        client.DefaultRequestHeaders.Add("x-apikey", ApiKey);
+
+        //        var response = await client.GetAsync("https://aeroapi.flightaware.com/aeroapi/airports/nearby?" +
+        //                "latitude=" + lat.ToString() +
+        //                "&longitude=" + lon.ToString() +
+        //                "&radius=" + radius);
+        //        var contentStream = await response.Content.ReadAsStreamAsync();
+        //        Debug.Log("RESPONSE CODE: " + response.StatusCode);
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            using (StreamReader reader = new StreamReader(contentStream))
+        //            {
+        //                string jsonstr = reader.ReadToEnd();
+        //                Debug.Log(jsonstr);
+        //                return JsonUtility.FromJson<AirportSet>(jsonstr);
+        //            }
+        //        }
+        //    }
+        //    return null;
+        //}
+        UnityWebRequest www = UnityWebRequest.Get("https://aeroapi.flightaware.com/aeroapi/airports/nearby?" +
+                        "latitude=" + lat.ToString() +
+                        "&longitude=" + lon.ToString() +
+                        "&radius=" + radius);
+        www.SetRequestHeader("x-apikey", ApiKey);
+        www.SetRequestHeader("Content-Type", "application/json");
+        yield return www.Send();
+        Debug.Log(www.result); Debug.Log(www.error);
+        return new AirportSet();
+    }
+
+    private IEnumerable getFromServer(string ApiKey, string url)
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://aeroapi.flightaware.com/aeroapi/airports/nearby?" +
+                        "latitude=" + lat.ToString() +
+                        "&longitude=" + lon.ToString() +
+                        "&radius=" + radius);
+        www.SetRequestHeader("x-apikey", ApiKey);
+        www.SetRequestHeader("Content-Type", "application/json");
+        yield return www.Send();
     }
 }
