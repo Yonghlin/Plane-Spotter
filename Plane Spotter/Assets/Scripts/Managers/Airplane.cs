358af3b8bead34061b6acab8a47fd8ada9466bf7
Devin <ds7133@engr.ship.edu>
2023-04-30 09:24:36 -0400

    add plane lines and arrow button

--- Plane Spotter/Assets/Scripts/Managers/Airplane.cs
+++ Plane Spotter/Assets/Scripts/Managers/Airplane.cs
@@ -16,11 +16,8 @@ public class Airplane : MonoBehaviour
 
     public double Heading;
     public double GroundSpeed;
-
-    private double longitudePrev;
-    private double latitudePrev;
-    private double altitudePrev;
-
+    public LineRenderer lineFront;
+    public LineRenderer lineBack;
     public GPS gps;
     public GeoConverter converter;
     public float distance_multiplier;
@@ -67,19 +64,38 @@ public class Airplane : MonoBehaviour
         posNew = posManager.NormalizePosition(posNew);
         posManager.SetBoundPosAndScale(this.gameObject, posNew);
     }
-    private void UpdateStoredCoordinates(float longitude, float altitude, float latitude)
-    {
-        longitudePrev = Longitude;
-        altitudePrev = Elevation;
-        latitudePrev = Latitude;
 
-        Longitude = longitude;
-        Elevation = altitude;
-        Latitude = latitude;
-    }
     public void UpdatePosition(float ElevationNew, float LatitudeNew, float LongitudeNew)
     {
-        UpdateStoredCoordinates(LongitudeNew, ElevationNew, LatitudeNew);
+        Longitude = LongitudeNew;
+        Elevation = ElevationNew;
+        Latitude = LatitudeNew;
+    }
+
+    private void Start()
+    {
+        // Initialize lines
+        LineRenderer[] lines = { lineFront, lineBack };
+        foreach (var line in lines)
+        {
+            line.positionCount = 2;
+            line.useWorldSpace = false;
+            line.startWidth = 0.3f;
+            line.endWidth = 0.3f;
+        }
+
+        // Front
+        lineFront.startColor = Color.blue;
+        lineFront.endColor = Color.blue;
+        lineFront.SetPosition(0, Vector3.zero);
+        lineFront.SetPosition(1, Vector3.forward * 1000f);
+        
+        // Back
+        lineBack.startColor = Color.green;
+        lineBack.endColor = Color.green;
+        lineBack.SetPosition(0, Vector3.zero);
+        lineBack.SetPosition(1, Vector3.back * 1000f);
+
     }
 
     // Update is called once per frame
